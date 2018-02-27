d104d1e0a5454c3d669a20f675a9d59ee6b85b89
snapPeas <patroclus_1@yahoo.com>
2018-02-27 11:39:18 -0500

    testing testfile

---
+++ packages/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/snapPeas/golangTraining/tree/master/packages/stringutil"
+)
+
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oGoG"))
+	fmt.Println(stringutil.MyName)
+}
